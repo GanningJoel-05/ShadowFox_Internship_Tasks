cfc554ed1f06715fdf841ddda32747226c666cca
Ganning Joel J <ganningjoel169@gmail.com>
2025-07-27 20:57:43 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,53 +1,73 @@
-package ContactManagement;
+package BankApp;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ContactManager manager = new ContactManager();
         Scanner scanner = new Scanner(System.in);
 
-        int choice;
+        System.out.println("\n========== WELCOME TO THE BANK ACCOUNT MANAGEMENT SYSTEM ==========");
 
-        System.out.println("\n========== WELCOME TO MY CONTACT MANAGEMENT SYSTEM 🧡 ==========");
+        System.out.print("Enter the Account Holder Name: \n");
+        String holderName = scanner.nextLine();
+        System.out.print("Enter the account number: \n");
+        String accountNumber = scanner.nextLine();
+        System.out.print("Enter the Bank Name: \n");
+        String bankName = scanner.nextLine();
+        System.out.print("Enter the initial balance (or 0): \n");
+        double initialBalance = scanner.nextDouble();
 
-        do {
-            System.out.println("\n========= MAIN MENU =========");
-            System.out.println("1. Add Contact");
-            System.out.println("2. View All Contacts");
-            System.out.println("3. Update Contact");
-            System.out.println("4. Delete Contact");
-            System.out.println("5. Search Contact");
+        BankAccount account = new BankAccount(holderName, accountNumber, bankName,initialBalance);
+
+        while (true) {
+            System.out.println("\n========== MAIN MENU ==========");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. View Transaction History");
+            System.out.println("5. Show Account Info");
             System.out.println("6. Exit");
-            System.out.print("Choose an option: \n");
-            choice = scanner.nextInt();
-            scanner.nextLine();
+            System.out.print("\nEnter your choice: \n");
+            int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    manager.addContact();
+                    System.out.println("\n========== DEPOSIT AMOUNT ==========");
+                    System.out.print("Enter the amount to be deposited: \n");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
                     break;
                 case 2:
-                    manager.viewContacts();
+                    System.out.println("\n========== WITHDRAWING AMOUNT ==========");
+                    System.out.print("Enter the amount to be withdrawn: \n");
+                    double withdrawAmount = scanner.nextDouble();
+                    account.withdraw(withdrawAmount);
                     break;
                 case 3:
-                    manager.updateContact();
+                    System.out.println("\n========== BALANCE AMOUNT ==========");
+                    System.out.println("Your Current Account Balance: ₹" + account.getBalance());
                     break;
                 case 4:
-                    manager.deleteContact();
+                    System.out.println("\n========== YOUR TRANSACTION HISTORY ==========");
+                    System.out.println("Transaction History:");
+                    for (Transaction transaction : account.getTransactionHistory()) {
+                        System.out.println(transaction);
+                    }
                     break;
                 case 5:
-                    manager.searchContact();
+                    System.out.println("\n========== YOUR ACCOUNT INFO ==========");
+                    System.out.println("Account Name: " + account.getAccountHolder());
+                    System.out.println("Account Number: " + account.getAccountNumber());
+                    System.out.println("Banking Name: " + account.getBankname());
                     break;
                 case 6:
-                    System.out.println("Exiting... 👋");
-                    System.out.println("Thanks a lot for using my Contact Management System! ❤️");
+                    System.out.println("\nExiting...... 👋");
+                    System.out.println("THANKS A LOT FOR USING MY CONSOLE BASED BANK ACCOUNT MANAGEMENT SYSTEM ❤️");
+                    System.exit(0);
                     break;
                 default:
-                    System.out.println("Invalid choice ❌. Try again.");
+                    System.out.println("Invalid choice ❌. Please try again.");
             }
         }
-        while (choice != 6);
-        scanner.close();
     }
 }
